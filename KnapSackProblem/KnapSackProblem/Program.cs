8f9531d7fdf7817f7bccf4a70f993d277293e34d
Patrick Thompson <thompsonpa2014@gmail.com>
2021-04-23 11:30:26 -0400

    Added List sort and started loop for finding best packages

--- KnapSackProblem/KnapSackProblem/Program.cs
+++ KnapSackProblem/KnapSackProblem/Program.cs
@@ -19,19 +19,34 @@ namespace KnapSackProblem
         static void Main(string[] args)
         {
             // Set Max Weight Amount Variable
-            double KnapSack = 15.0;
+            double KnapSack = 10.0;
 
             // Set Items List variable
             List<Item> Items = new List<Item>();
 
             // Add Items to List
-            Items.Add(new Item() { weight=2.5, value= 5.0 });
+            Items.Add(new Item() { weight = 2.5, value = 5.0 });
             Items.Add(new Item() { weight = 3.5, value = 4.0 });
             Items.Add(new Item() { weight = 1.5, value = 2.0 });
-            Items.Add(new Item() { weight = 10.0, value = 3.0 });
-            Items.Add(new Item() { weight = 3.0, value = 10.0 });
+            Items.Add(new Item() { weight = 10.0, value = 5.0 });
+            Items.Add(new Item() { weight = 5.0, value = 10.0 });
+            Items.Add(new Item() { weight = 16.0, value = 9.0 });
+            Items.Add(new Item() { weight = 12.0, value = 8.0 });
 
             // Have Items and Knapsack set up, need to solve problem on which combo will be the best value
+            // first organize the list Largest First (Descending)
+            Items.Sort();
+            Items.Reverse();
+
+
+            // Need to create formula to create an over all index value for each item
+            foreach (var Package in Items)
+            {
+                if (Package.weight <= KnapSack)
+                {
+
+                }
+            }
 
             Console.WriteLine("Hello World, It works");
             Console.ReadLine();
